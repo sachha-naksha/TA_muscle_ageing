1b12c0c48ece1423b657dbbc162d28b83f09d190
akanksha-sachan <asachan2@andrew.cmu.edu>
2026-01-15 17:10:17 -0500

    running slide with higher spec 0.2 for male cells

--- R_scripts/slide_runs.R
+++ R_scripts/slide_runs.R
@@ -1,11 +1,23 @@
+# Install required packages if not already installed
+if (!require("devtools")) install.packages("devtools")
+if (!require("yaml")) install.packages("yaml")
+
+# Load libraries
 library(devtools)
+library(yaml)
+
+# Install SLIDE from GitHub
 devtools::install_github("jishnu-lab/SLIDE")
 
 
-yaml_path = "/ix/djishnu/alw399/SLIDE_py/example2/params.yaml"
+yaml_path = "/ix/djishnu/Akanksha/analysis_code/snRNA_TA_muscle_analysis/TA_muscle_code/R_scripts/config.yaml"
 input_params <- yaml::yaml.load_file(yaml_path)
 SLIDE::checkDataParams(input_params)
 
 SLIDE::optimizeSLIDE(input_params, sink_file = FALSE)
 
 SLIDE::plotCorrelationNetworks(input_params)
+
+# Run full CV after selecting delta and lambda
+
+# SLIDE::SLIDEcv(yaml_path, nrep = 2000, k = 20)
